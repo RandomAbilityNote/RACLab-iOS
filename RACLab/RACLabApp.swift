0a6f7615a42c2dcf300236feeee7028da66d4428
yongbeomkwak <yongbeomkwak@gmail.com>
2025-02-23 20:32:10 +0900

    Initial Commit

---
+++ RACLab/RACLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  RACLabApp.swift
+//  RACLab
+//
+//  Created by yongbeomkwak on 2/23/25.
+//
+
+import SwiftUI
+
+@main
+struct RACLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
