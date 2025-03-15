db80c2f681bb6767f1066a60776a01da81be7794
yongbeomkwak <yongbeomkwak@gmail.com>
2025-03-15 14:13:23 +0900

    feat: Hstack 제거

--- SearchFeature/SearchFeature/Implementation/SearchView.swift
+++ SearchFeature/SearchFeature/Implementation/SearchView.swift
@@ -24,17 +24,16 @@ struct SearchView: View {
           Image(uiImage: tribe.cardImage)
             .resizable()
             .padding(.top, 25)
-          HStack {
-            VStack(spacing: -4) {
-              Text(tribe.rawValue)
-                .font(.pretendard(size: 22, weight: .medium))
-                .foregroundStyle(.white)
-              Text(tribe.engName)
-                .font(.pretendard(size: 14, weight: .regular))
-                .foregroundStyle(Color.ColorSystem.beige)
-            }
-            .frame(maxWidth: .infinity) // 텍스트를 중앙 정렬
+          VStack(spacing: -4) {
+            Text(tribe.rawValue)
+              .font(.pretendard(size: 22, weight: .medium))
+              .foregroundStyle(.white)
+            Text(tribe.engName)
+              .font(.pretendard(size: 14, weight: .regular))
+              .foregroundStyle(Color.ColorSystem.beige)
           }
+          .frame(maxWidth: .infinity) // 텍스트를 중앙 정렬
+
           .overlay(content: {
             HStack {
               Spacer()
